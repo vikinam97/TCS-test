043d8c7a5f7d0fab491a7f0ecdb345b2f51b61aa
vikinam97 <vikinam97@gmail.com>
2018-10-15 17:19:52 +0100

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component,Headers} from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
 
+import { ServiceProvider } from '../../providers/service/service';
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  user: String,
+  pass: String
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,private serviceProvider:ServiceProvider) {
 
   }
+  login()
+  {
+  this.serviceProvider.getdate()
+  .subscribe(res=>{console.log(res);});
 
+  }
 }
---
+++ src/providers/service/service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the ServiceProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class ServiceProvider {
+
+  constructor(public http: HttpClient) {
+    //console.log('Hello ServiceProvider Provider');
+  }
+
+  getdate()
+  {
+  	return this.http.get("http://date.jsontest.com/");
+  }
+
+}
